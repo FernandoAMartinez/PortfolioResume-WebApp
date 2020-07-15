a16eb37b87e3e6abd5bb1955714c63f07f55e6a2
Fernando Martinez <39782229+FernandoAMartinez@users.noreply.github.com>
2020-07-15 02:07:31 -0300

    Add project files.

---
+++ PortfolioApplication/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PortfolioApplication.Models;
+
+namespace PortfolioApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        #region Views
+        public IActionResult Index() => View();
+
+        [Route("/{action}")]
+        public IActionResult Privacy() => View();
+        
+        #endregion
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
